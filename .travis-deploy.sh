d21d9b7a8d7c0f48eee71c1d4893f92165147526
Kyra <kyradiscord@gmail.com>
2019-01-30 00:31:08 +0100

    ci: Removed docs script

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -12,37 +12,13 @@ fi
 # For pull requests, do nothing
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Not building for a non branch push - building without deploying."
-  yarn docs
   exit 0
-fi
-
-# If it's not a tag (and not a pull request push)
-if [ -z $TRAVIS_TAG ]; then
-  echo -e "Building for a branch push - building and deploying."
-
-  SOURCE="${TRAVIS_BRANCH//\/|\./_/}"
-  REPO=$(git config remote.origin.url)
-  SHA=$(git rev-parse --verify HEAD)
-
-  # Docs
-  TARGET_BRANCH="docs"
-  git clone $REPO out -b $TARGET_BRANCH
-  yarn docs
-  mv docs/docs.json out/${SOURCE}.json
-
-  cd out
-  git add --all .
-  git config user.name "Travis CI"
-  git config user.email "$COMMIT_AUTHOR_EMAIL"
-  git commit -m "Docs build: ${SHA}" || true
-  git push "https://${GH_TOKEN}@${GH_REF}" $TARGET_BRANCH
 fi
 
 # Only push to webpack if it's a release or master branch
 if [ -n "$TRAVIS_TAG" ] || [ $TRAVIS_BRANCH == "master" ]; then
-  # Clean-up
-  cd ..
-  rm -rf out
+  REPO=$(git config remote.origin.url)
+  SHA=$(git rev-parse --verify HEAD)
 
   # Do the thing once more for webpack
   TARGET_BRANCH="webpack"
